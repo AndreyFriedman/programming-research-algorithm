f142ed583ebfe083e05a930276ee5f3566cf7756
AndreyFriedman <andrey.fridman2001@gmail.com>
2022-08-14 20:56:46 +0300

    update init

--- algo_flask/__init__.py
+++ algo_flask/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import sys
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ecf6e975838a2f7bf3c5dbe7d55ebe5b'
+sys.path.append('/home/AndreyFridman0/algo_research') # import algo_research module from server
 from algo_flask import routes
